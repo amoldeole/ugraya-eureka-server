b4bf4ecd3b1dd804959dc52ae1e5198ed63c14d4
amoldeole <amoldeole511@gmail.com>
2022-08-19 03:20:05 +0530

    Ugraya-1: Initial commit

---
+++ src/test/java/eurekaserver/adeole/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package eurekaserver.adeole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
